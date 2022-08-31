2a0d2af5b64cc297b58180bbde416003f6272947
pastjacket <log.deboo@gmail.com>
2022-08-30 21:16:17 -0700

    Dockerized!

--- Back_end/flask-server.py
+++ Back_end/flask-server.py
@@ -16,7 +16,7 @@ seconds_in_day = 86400
 
 
 cache = Cache(config={"CACHE_TYPE": "RedisCache",
-                      "CACHE_REDIS_HOST": "0.0.0.0",
+                      "CACHE_REDIS_HOST": "db",
                       "CACHE_REDIS_PORT": 6379,
                       "CACHE_DEFAULT_TIMEOUT": -1})
 
@@ -34,44 +34,44 @@ def create_app():
     def post_politics():
         print("politics routed")
         if cache.get('Politics') is None:
-            cache.set('Politics', getArticleFeed(politics, 4))
+            cache.set('Politics', getArticleFeed(politics, 1))
         return cache.get("Politics")
 
     @app.route('/Business')
     def post_Business():
         if cache.get('Business') is None:
-            cache.set('Business', getArticleFeed(business, 4))
+            cache.set('Business', getArticleFeed(business, 0))
         return cache.get("Business")
 
     @app.route('/Health')
     def post_Health():
         if cache.get('Health') is None:
-            cache.set('Health', getArticleFeed(health, 4))
+            cache.set('Health', getArticleFeed(health, 0))
         return cache.get("Health")
 
     @app.route('/Entertainment')
     def post_Entertainment():
         if cache.get('Entertainment') is None:
             cache.set('Entertainment', getArticleFeed(
-                arts_and_entertainment, 4))
+                arts_and_entertainment, 0))
         return cache.get("Entertainment")
 
     @app.route('/Science')
     def post_Science():
         if cache.get('Science') is None:
-            cache.set('Science', getArticleFeed(science, 4))
+            cache.set('Science', getArticleFeed(science, 0))
         return cache.get("Science")
 
     @app.route('/Technology')
     def post_Technology():
         if cache.get('Technology') is None:
-            cache.set('Technology', getArticleFeed(technology, 4))
+            cache.set('Technology', getArticleFeed(technology, 0))
         return cache.get("Technology")
 
     @app.route('/Environment')
     def post_Environment():
         if cache.get('Environment') is None:
-            cache.set('Environment', getArticleFeed(environment, 4))
+            cache.set('Environment', getArticleFeed(environment, 0))
         return cache.get("Environment")
 
     if __name__ == "__main__":
