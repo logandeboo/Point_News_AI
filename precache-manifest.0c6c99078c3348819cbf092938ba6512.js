f75054414326040207da6ff0a890f69ae117b7fc
pastjacket <log.deboo@gmail.com>
2022-09-02 14:23:41 -0700

    Updates

---
+++ precache-manifest.0c6c99078c3348819cbf092938ba6512.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "abcf6488a865bf6c24db",
+    "url": "/Point_News_AI/static/css/main.cf505366.chunk.css"
+  },
+  {
+    "revision": "abcf6488a865bf6c24db",
+    "url": "/Point_News_AI/static/js/main.abcf6488.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/Point_News_AI/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "1fb84335d4ce8f26b1b6",
+    "url": "/Point_News_AI/static/js/2.1fb84335.chunk.js"
+  },
+  {
+    "revision": "f343b80c7ef29a6daefa",
+    "url": "/Point_News_AI/static/js/runtime~main.f343b80c.js"
+  },
+  {
+    "revision": "cce93e251e67ee56c2f210734a70f95b",
+    "url": "/Point_News_AI/index.html"
+  }
+];
