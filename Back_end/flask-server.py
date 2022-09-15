8516d475179a3dff7f07bf63fdbfedf31bf7cb88
pastjacket <log.deboo@gmail.com>
2022-09-15 12:51:34 -0700

    Server now properly handles empty initialization

--- Back_end/flask-server.py
+++ Back_end/flask-server.py
@@ -9,9 +9,6 @@ from flask_apscheduler import APScheduler
 routes = ['Politics', 'Business', 'Health',
           'Entertainment', 'Science', 'Technology', 'Environment']
 
-# routes = ['Politics']
-
-
 seconds_in_day = 86400
 
 
@@ -29,78 +26,64 @@ def create_app():
 
     @app.route('/Politics')
     def get_politics():
-        print("Politics route hit")
+        if cache.get("Politics") is None:
+            getArticleFeed("news/Politics", 1)
         return cache.get("Politics")
 
     @app.route('/Business')
     def get_Business():
+        if cache.get("Business") is None:
+            getArticleFeed("news/Business", 1)
         return cache.get("Business")
 
     @app.route('/Health')
     def get_Health():
+        if cache.get("Health") is None:
+            getArticleFeed("news/Health", 1)
         return cache.get("Health")
 
     @app.route('/Entertainment')
     def get_Entertainment():
+        if cache.get("Entertainment") is None:
+            getArticleFeed("news/Entertainment", 1)
         return cache.get("Entertainment")
 
     @app.route('/Science')
     def get_Science():
+        if cache.get("Science") is None:
+            getArticleFeed("news/Science", 1)
         return cache.get("Science")
 
     @app.route('/Technology')
     def get_Technology():
+        if cache.get("Technology") is None:
+            getArticleFeed("news/Technology", 1)
         return cache.get("Technology")
 
     @app.route('/Environment')
     def get_Environment():
+        if cache.get("Environment") is None:
+            getArticleFeed("news/Environment", 1)
         return cache.get("Environment")
 
     def scheduledUpdate():
-        print("Updating")
 
         for route in routes:
             # retrieve new article to be added to feed
             temp = json.loads(getArticleFeed("news/" + route, 1))
 
             # append contents of current JSON array into new JSON array, which starts with the new article
-            # contents will be null upon server initialization
-            if cache.get(route) is not None:
-                for item in json.loads(cache.get(route)):
-                    temp.append(item)
+            for item in json.loads(cache.get(route)):
+                temp.append(item)
 
             cache.set(route, json.dumps(
                 temp, default=lambda o: o.__dict__, indent=4))
 
-        # json.loads on cache returns array
-        # current = json.loads(cache.get('Environment'))
-        # for item in json.loads(cache.get('Environment')):
-        #     current.append(item)
-        # new = addition + current
-        # cache.set('Politics', new)
-        # print(current)
-
     if __name__ == "__main__":
-        # scheduler.add_job(id='Scheduled Update',
-        #                   func=scheduledUpdate, trigger='interval', seconds=45)
-        # scheduler.start()
+        scheduler.add_job(id='Scheduled Update',
+                          func=scheduledUpdate, trigger='interval', seconds=45)
+        scheduler.start()
         app.run(debug=False, host="0.0.0.0", port=5001)
-
-
-# Testing update method
-# There should be a scheduled update method that does the "putting"
-# The routes should exclusively be "getting"
-# --------------------------------------------
-# TODO:
-# - Write method to update cache
-# - Use APScheduler to schedule said method
-# REMEMBER TO CHANGE REDIS HOST AFTER TESTING
-
-# TODO Updated
-# - No 'update' strategy is present in rediscache operators Sooo..
-# The solution is to get the route dictionary, append an new article(s) to it, then set the updated value
-# json.dumps - python object -> json string
-# json.loads - json string -> python object
 
 
 create_app()
